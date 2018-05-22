e861f552aa62046ca98d3191628d089fa2abf755
lidi <lidi0903@163.com>
2018-05-23 00:02:49 +0800

    finish user curd

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,9 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except'=>['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create'],
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(2);
+        return view('users.index', compact('users'));
+    }
+
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      * 用戶註冊
@@ -25,6 +44,11 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * 注册
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -41,5 +65,32 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    /**
+     * @param User $user
+     * 用户编辑
+     */
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
     }
 }
